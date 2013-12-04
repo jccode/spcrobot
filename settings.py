99d8e033956a6e724eafb035f7c9282ccc5c7003
jcchen <junchangchen@gmail.com>
2013-12-04 20:16:16 +0800

    add settings.py. support generate evidence.

---
+++ settings.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+CR_FOLDER = 'd:/HGST/MFG/processing/HDD_WEBSPC_CR/C144/'
+
+SPECIFICATION_XLS = CR_FOLDER + 'HDD SPC Monitoring Parameter Specification rev.4.1_jc.xls'
+
+MASTER_XLS = CR_FOLDER + 'HDD SPC Monitoring Parameter Specification rev.4.1.xlsx'
+
+SPCIDS_FILE = CR_FOLDER + 'spcids.txt'
+
+MASTER_PROFILEIDS_FILE = CR_FOLDER + 'profileIds.txt'
+
+DATASOURCE_FILE = "datasource.ini"
+
+
+LOG_DIR = CR_FOLDER + "log"
+SQLRET_DIR = CR_FOLDER + 'sqlret'
+
+UT_LOG = CR_FOLDER + 'ut.log'
+
